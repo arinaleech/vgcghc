3f9c1affc85da0bc1d0da7d4a845cd9494b48160
arinaleech <ndjdn6062@gmail.com>
2025-01-09 19:27:11 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modified by LISA-KOREA | @LISA_FAN_LK
-
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import asyncio
 import json
+import subprocess
 import math
 import os
 import shutil
 import time
 from datetime import datetime
-
+from pyrogram import enums 
 from plugins.config import Config
-from plugins.translation import Translation
-from plugins.custom_thumbnail import *
+from plugins.script import Translation
+from plugins.thumbnail import *
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types import InputMediaPhoto
-from functions.display_progress import progress_for_pyrogram, humanbytes
+from plugins.functions.display_progress import progress_for_pyrogram, humanbytes
 from plugins.database.database import db
 from PIL import Image
-from functions.ran_text import random_char
+from plugins.functions.ran_text import random_char
+
+
+#cookies_file = "cookies.txt"
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
