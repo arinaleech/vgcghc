d45bac08bfa75cc3c78cdf5b8e75df7479eae0a0
arinaleech <ndjdn6062@gmail.com>
2025-01-09 16:39:01 +0530

    Update forcesub.py

--- plugins/functions/forcesub.py
+++ plugins/functions/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/NUPPORT).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_to_message_id=message.id,
@@ -39,11 +39,9 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/NTUPPORT).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_to_message_id=message.id,
         )
         return 400
-
-
